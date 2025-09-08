a6a9c58ca4d85d5206e6ac6588999c6a6c087771
saketkarnik79 <saket@optimistikinfo.com>
2025-09-08 20:58:04 +0530

    Day6

---
+++ Demos/AZ_DemoCosmosDB/AZ_DemoCosmosDB/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZ_DemoCosmosDB.Models
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("category")]
+        public string? Category { get; set; }
+
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public bool Clearance { get; set; }
+    }
+}
